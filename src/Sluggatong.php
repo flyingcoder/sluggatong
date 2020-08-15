97efd3d3f17133d0b4dfcaa244439b4f0a1253b3
Alvin <aepacot2016@gmail.com>
2020-08-15 23:18:53 +0800

    Slug class for personal use

---
+++ src/Sluggatong.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Flyingcoder\Sluggatong;
+
+class Slug {
+
+    protected $model;
+    protected $column;
+
+    public function __construct($model, $column)
+    {
+        $this->column = $column;
+
+        $nameSpace = '\\App\\';
+
+        $entity = app(ucfirst($model));
+
+        if(class_exists($nameSpace . $entity))
+            $this->model = $nameSpace . $entity;
+        else
+            throw new \Exception('Model class does not exists.');
+    }
+
+    /**
+     * @param $title
+     * @param int $id
+     * @return string
+     * @throws \Exception
+     */
+    public function createSlug($title, $id = 0)
+    {
+        // Normalize the title
+        $slug = str_slug($title);
+
+        // Get any that could possibly be related.
+        // This cuts the queries down by doing it once.
+        $allSlugs = $this->getRelatedSlugs($slug, $id);
+
+        // If we haven't used it before then we are all good.
+        if (! $allSlugs->contains('slug', $slug)){
+            return $slug;
+        }
+
+        // Just append numbers like a savage until we find not used.
+        for ($i = 1; $i <= 10; $i++) {
+            $newSlug = $slug.'-'.$i;
+            if (! $allSlugs->contains('slug', $newSlug)) {
+                return $newSlug;
+            }
+        }
+
+        throw new \Exception('Can not create a unique slug');
+    }
+
+    protected function getRelatedSlugs($slug, $id = 0)
+    {
+
+        return $this->model->select($this->column)->where($this->column, 'like', $slug.'%')
+                           ->where('id', '<>', $id)
+                           ->get();
+    }
+}
